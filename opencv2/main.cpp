5856c8cc2d0f73df0bc2746d9234f500844aedad
junghwalee <dlwdjghk06@gmail.com>
2025-07-14 03:08:17 +0900

    Modify Palm detection logic

--- opencv2/main.cpp
+++ opencv2/main.cpp
@@ -12,8 +12,8 @@ int main() {
 
     cv::Mat frame, gray, avgFloat, avgGray, diff, thresh;
     bool isAvgInit = false;
-    const int MOTION_THRESHOLD = 10;
-    const int MOTION_TRIGGER_SCORE = 5;
+    const int MOTION_THRESHOLD = 6;
+    const int MOTION_TRIGGER_SCORE = 4;
     const int DISPLAY_TEXT_FRAMES = 3000;
 
     int motionScore = 0;
@@ -22,17 +22,24 @@ int main() {
     int prevMinX = -1;
     int prevMaxX = -1;
 
+    // STOP 인식 -----------
     bool palmDetected = false;
     int lastMotionArea = 0;
+    int palmStableCounter = 0;
+
+    const float PALM_AREA_RATIO_THRESHOLD = 2.5f;
+    const int PALM_MIN_AREA = 10000;
+    const int PALM_RESET_FRAME_COUNT = 30;
+    // -------------------
 
     // threshold 조절
     int threshold_value = 40;
-    // 윈도우 생성
     cv::namedWindow("Thresh");
-    // 트랙바 생성
     cv::createTrackbar("Threshold", "Thresh", &threshold_value, 100); // 0 ~ 100 조절 가능
+    // -----------------------
 
     while (true) {
+        bool justDetected = false;
         cap >> frame;
         if (frame.empty()) break;
 
@@ -70,7 +77,27 @@ int main() {
             }
             //std::cout << "min: " << minX << ", max: " << maxX << std::endl;
 
+            // 손바닥 판정
             int area = motionPoints.size();
+            // 손바닥 등장 판단: area가 갑자기 커졌을 때, 일정 이상일 때
+            if (!palmDetected && lastMotionArea > 0) {
+                float areaRatio = (float)area / lastMotionArea;
+                if (areaRatio > PALM_AREA_RATIO_THRESHOLD && area > PALM_MIN_AREA) {
+                    std::cout << "STOP" << std::endl;
+                    palmDetected = true;
+                    palmStableCounter = 0;
+                }
+            }
+
+            // 손바닥 유지 시간 체크
+            if (palmDetected) {
+                palmStableCounter++;
+                if (palmStableCounter > PALM_RESET_FRAME_COUNT) {
+                    palmDetected = false;
+                    palmStableCounter = 0;
+                }
+            }
+            /*
             if (!palmDetected && lastMotionArea < 5000 && area > 12000) {
                 std::cout << "STOP" << std::endl;
                 palmDetected = true;
@@ -79,38 +106,55 @@ int main() {
             if (palmDetected && area < 3000) {
                 palmDetected = false;
             }
-
+            */
             lastMotionArea = area;
 
-            // left / right swipe 판정
-            if (prevMinX != -1 && prevMaxX != -1) {
-                int dxMax = maxX - prevMaxX;
-                int dxMin = minX - prevMinX;
-                // std::cout << dxMin << ", " << dxMax << std::endl;
-                if (dxMax > MOTION_THRESHOLD) {
-                    motionScore = (motionScore >= 0) ? motionScore + 1 : 0;
-                }
-                else if (dxMin < -MOTION_THRESHOLD) {
-                    motionScore = (motionScore <= 0) ? motionScore - 1 : 0;
-                }
-                else {
-                    motionScore = 0;
-                }
+            // left / right swipe 판정 (손바닥 상태 아닐때만)
+            if (!palmDetected) {
+                if (prevMinX != -1 && prevMaxX != -1) {
+                    int dxMax = maxX - prevMaxX;
+                    int dxMin = minX - prevMinX;
+                    std::cout << dxMin << ", " << dxMax << std::endl;
+                    if (dxMax > MOTION_THRESHOLD) {
+                        motionScore = (motionScore >= 0) ? motionScore + 1 : 0;
+                    }
+                    else if (dxMin < -MOTION_THRESHOLD) {
+                        motionScore = (motionScore <= 0) ? motionScore - 1 : 0;
+                    }
+                    else {
+                        //motionScore = 0;
+                        if (motionScore > 0) motionScore--;
+                        else if (motionScore < 0) motionScore++;
+                    }
 
-                if (motionScore >= MOTION_TRIGGER_SCORE) {
-                    std::cout << "LEFT" << std::endl;
-                    motionScore = 0;
+                    if (motionScore >= MOTION_TRIGGER_SCORE) {
+                        std::cout << "LEFT" << std::endl;
+                        motionScore = 0;
+                        prevMinX = -1;
+                        prevMaxX = -1;
+                        justDetected = true;
+                    }
+                    else if (motionScore <= -MOTION_TRIGGER_SCORE) {
+                        std::cout << "RIGHT" << std::endl;
+                        motionScore = 0;
+                        prevMinX = -1;
+                        prevMaxX = -1;
+                        justDetected = true;
+                    }
                 }
-                else if (motionScore <= -MOTION_TRIGGER_SCORE) {
-                    std::cout << "RIGHT" << std::endl;
-                    motionScore = 0;
+                if (!justDetected) {
+                    prevMinX = minX;
+                    prevMaxX = maxX;
                 }
             }
-            prevMinX = minX;
-            prevMaxX = maxX;
+            else {
+                prevMinX = minX;
+                prevMaxX = maxX;
+            }
         }
         else {
-            motionScore = 0;
+            // 손 없을 때만 점수 초기화
+            if (!palmDetected)   motionScore = 0;
         }
 
         // 7. 영상 출력
